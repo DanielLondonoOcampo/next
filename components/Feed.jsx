c12cc33cd1f5eb210a8e303f6409a6d32fd9c2e1
Daniel Londono <daniel.londono.o@hotmail.com>
2023-09-25 23:16:14 -0300

    "added search feature"

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -21,21 +21,31 @@ export const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
 
-  const handleSearchChange = (e) => {};
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
 
   useEffect(() => {
     const fetchPrompts = async () => {
       const response = await fetch("api/prompt");
       const data = await response.json();
+      const regex = new RegExp(searchText, "i");
       setPosts(data);
+      searchText &&
+        setPosts(
+          data.filter((post) => regex.test(post.prompt) || regex.test(post.tag))
+        );
     };
 
     fetchPrompts();
-  }, []);
+  }, [searchText]);
 
   return (
     <section className="feed">
-      <form className="relative w-full flex-center">
+      <form
+        className="relative w-full flex-center"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
           type="text"
           placeholder="Search for a tag or username"
